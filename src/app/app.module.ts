e583506d8f31431dfe0fe6050fc83b0cb16ea3ee
RoseMagura <magurarm@guilford.edu>
2021-01-12 14:05:50 -0500

    Feat: Add styling and data from Udacity starter code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ProductListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
