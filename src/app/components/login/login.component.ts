5a80d3bbca820d79b83c8faa3bc99964209420a1
RoseMagura <magurarm@guilford.edu>
2021-02-10 15:07:22 -0500

    Feat: Working on logging in with Post

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,54 +1,41 @@
 import { Component } from '@angular/core';
-import { Output, EventEmitter } from '@angular/core';
 import { User } from '../../interfaces/User';
 import { UserService } from '../../services/user.service';
-import * as bcrypt from 'bcrypt';
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   constructor(private userService: UserService) {}
 
-  users: User[];
   user: User;
   signingIn = true;
   creating = false;
-  
-  @Output() loginEvent = new EventEmitter<string>();
-
-  ngOnInit(): void {
-      this.getUsers();
-  }
-
-  getUsers(): void {
-    this.userService.getUsers().subscribe((users) => (this.users = users));
-  }
+  authMsg: string;
+  error = false;
 
   onSubmit(loginForm: any): void {
     console.log(loginForm);
-    console.log(loginForm.firstName);
-    this.users.forEach(async (user) => {
-        if(user.first_name === loginForm.firstName && user.last_name === loginForm.lastName){
-            console.log('MATCH');
-            const hashedPwd = await bcrypt.hash(loginForm.password, 10);
-            // console.log(hashedPwd);
-            // console.log(user.password === hashedPwd);
-        };
-    });
-
+    this.userService
+      .login(loginForm)
+      .subscribe((response) => {
+        console.log(response.body['message'], response.body['success']);
+        this.error = !response.body['success'];
+        this.authMsg = response.body['message'];
+      }
+          );
   }
 
   createUser(): void {
-      this.signingIn = false;
-      this.creating = true;
-      console.log(this.creating);
+    this.signingIn = false;
+    this.creating = true;
+    console.log(this.creating);
   }
-  
-  onPostUser(form: any): void{
-    console.log('CREATING USER', form);
 
+  onPostUser(form: any): void {
+    console.log('CREATING USER', form);
   }
+
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/User';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-
-private dataUrl = 'http://localhost:3000/users?accept';
+  private dataUrl = 'http://localhost:3000/';
 
   constructor(private http: HttpClient) {}
 
-  test() {
-    console.log(this.http.get(this.dataUrl));
-  }
-
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.dataUrl);
+  login(user: User) {
+    return this.http.post(this.dataUrl, user, {
+      observe: 'response',
+      withCredentials: true
+    });
   }
 }
